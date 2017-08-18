ce8a8fd0683649afa58471c79113a5bd02c7952b
Sumit Tungdenjing <Sumit.Tungdenjing@thomsonreuters.com>
2017-08-18 15:15:40 +0700

    Fix error in IE with dev tool

--- js/test-container.js
+++ js/test-container.js
@@ -1,50 +1,52 @@
-var html = '<h1>Hello from test-container</h1><div style="border: 1px solid red"><test-element></test-element></div>';
-var domHtml = '<dom-module id="test-container"></dom-module>';
-//document.head.insertAdjacentHTML('beforeend', domHtml);
-//var cModule = document.head.lastChild;
-var template2 = document.createElement('template');
-template2.innerHTML = html;
-//cModule.appendChild(cTemplate);
+window.addEventListener('WebComponentsReady', function () {
+  var html = '<h1>Hello from test-container</h1><div style="border: 1px solid red"><test-element></test-element></div>';
+  var domHtml = '<dom-module id="test-container"></dom-module>';
+  //document.head.insertAdjacentHTML('beforeend', domHtml);
+  //var cModule = document.head.lastChild;
+  var template2 = document.createElement('template');
+  template2.innerHTML = html;
+  //cModule.appendChild(cTemplate);
 
-console.info('Declaring test-container');
+  console.info('Declaring test-container');
 
-var TestContainer = function (a) {
+  var TestContainer = function (a) {
     function b() {
-        babelHelpers.classCallCheck(this, b);
-        var c = babelHelpers.possibleConstructorReturn(this, (b.__proto__ || Object.getPrototypeOf(b)).call(this));
-        return console.info(b.is + ': Created'), c.prop1 = 'Created', c
+      babelHelpers.classCallCheck(this, b);
+      var c = babelHelpers.possibleConstructorReturn(this, (b.__proto__ || Object.getPrototypeOf(b)).call(this));
+      return console.info(b.is + ': Created'), c.prop1 = 'Created', c
     }
     return babelHelpers.inherits(b, a), babelHelpers.createClass(b, null, [{
-        key: 'is',
-        get: function () {
-            return 'test-container'
-        }
+      key: 'is',
+      get: function () {
+        return 'test-container'
+      }
     }, {
-        key: 'template',
-        get: function () {
-            return template2;
-        }
+      key: 'template',
+      get: function () {
+        return template2;
+      }
     }, {
-        key: 'properties',
-        get: function () {
-            return {
-                prop1: {
-                    type: String,
-                    value: 'Not Ready!'
-                }
-            }
+      key: 'properties',
+      get: function () {
+        return {
+          prop1: {
+            type: String,
+            value: 'Not Ready!'
+          }
         }
+      }
     }]), babelHelpers.createClass(b, [{
-        key: 'connectedCallback',
-        value: function () {
-            babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'connectedCallback', this).call(this), console.info(b.is + ': Connected Callback'), this.prop1 = 'Connected Callback'
-        }
+      key: 'connectedCallback',
+      value: function () {
+        babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'connectedCallback', this).call(this), console.info(b.is + ': Connected Callback'), this.prop1 = 'Connected Callback'
+      }
     }, {
-        key: 'ready',
-        value: function () {
-            babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'ready', this).call(this), console.info(b.is + ': Ready'), this.prop1 = 'Ready'
-        }
+      key: 'ready',
+      value: function () {
+        babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'ready', this).call(this), console.info(b.is + ': Ready'), this.prop1 = 'Ready'
+      }
     }]), b
-}(Polymer.Element);
+  }(Polymer.Element);
 
-window.customElements.define(TestContainer.is, TestContainer);
+  window.customElements.define(TestContainer.is, TestContainer);
+});
--- js/test-element.js
+++ js/test-element.js
@@ -1,50 +1,52 @@
-var html = '<h1>Hello from test-element</h1><p>[[prop1]]</p>';
-var domHtml = '<dom-module id="test-element"></dom-module>';
-//document.head.insertAdjacentHTML('beforeend', domHtml);
-//var cModule = document.head.lastChild;
-var template1 = document.createElement('template');
-template1.innerHTML = html;
-//cModule.appendChild(cTemplate);
+window.addEventListener('WebComponentsReady', function () {
+  var html = '<h1>Hello from test-element</h1><p>[[prop1]]</p>';
+  var domHtml = '<dom-module id="test-element"></dom-module>';
+  //document.head.insertAdjacentHTML('beforeend', domHtml);
+  //var cModule = document.head.lastChild;
+  var template1 = document.createElement('template');
+  template1.innerHTML = html;
+  //cModule.appendChild(cTemplate);
 
-console.info('Declaring test-element');
+  console.info('Declaring test-element');
 
-var TestElement = function (a) {
+  var TestElement = function (a) {
     function b() {
-        babelHelpers.classCallCheck(this, b);
-        var c = babelHelpers.possibleConstructorReturn(this, (b.__proto__ || Object.getPrototypeOf(b)).call(this));
-        return console.info(b.is + ': Created'), c.prop1 = 'Created', c
+      babelHelpers.classCallCheck(this, b);
+      var c = babelHelpers.possibleConstructorReturn(this, (b.__proto__ || Object.getPrototypeOf(b)).call(this));
+      return console.info(b.is + ': Created'), c.prop1 = 'Created', c
     }
     return babelHelpers.inherits(b, a), babelHelpers.createClass(b, null, [{
-        key: 'is',
-        get: function () {
-            return 'test-element'
-        }
+      key: 'is',
+      get: function () {
+        return 'test-element'
+      }
     }, {
-        key: 'template',
-        get: function () {
-            return template1;
-        }
+      key: 'template',
+      get: function () {
+        return template1;
+      }
     }, {
-        key: 'properties',
-        get: function () {
-            return {
-                prop1: {
-                    type: String,
-                    value: 'Not Ready!'
-                }
-            }
+      key: 'properties',
+      get: function () {
+        return {
+          prop1: {
+            type: String,
+            value: 'Not Ready!'
+          }
         }
+      }
     }]), babelHelpers.createClass(b, [{
-        key: 'connectedCallback',
-        value: function () {
-            babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'connectedCallback', this).call(this), console.info(b.is + ': Connected Callback'), this.prop1 = 'Connected Callback'
-        }
+      key: 'connectedCallback',
+      value: function () {
+        babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'connectedCallback', this).call(this), console.info(b.is + ': Connected Callback'), this.prop1 = 'Connected Callback'
+      }
     }, {
-        key: 'ready',
-        value: function () {
-            babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'ready', this).call(this), console.info(b.is + ': Ready'), this.prop1 = 'Ready'
-        }
+      key: 'ready',
+      value: function () {
+        babelHelpers.get(b.prototype.__proto__ || Object.getPrototypeOf(b.prototype), 'ready', this).call(this), console.info(b.is + ': Ready'), this.prop1 = 'Ready'
+      }
     }]), b
-}(Polymer.Element);
+  }(Polymer.Element);
 
-window.customElements.define(TestElement.is, TestElement);
+  window.customElements.define(TestElement.is, TestElement);
+});
